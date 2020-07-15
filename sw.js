3decebf4e2cfe348e762f768347de5a19274accf
Lupilgaming <65975615+Lupilgaming@users.noreply.github.com>
2020-07-15 13:25:26 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+//installation
+self.addEventListener('install', evt => {
+	// console.log('service worker installed');
+});
+
+//activation
+self.addEventListener('activate', evt => {
+	// console.log('service worker activated');
+});
+
+//fetching
+self.addEventListener('fetch', evt => {
+	// console.log('fetching ', evt);
+});
